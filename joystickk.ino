83fed99903ffb38bbb32129c0b1dd8ca2f526fc5
Keerthana <131702504+keerthanas2004@users.noreply.github.com>
2024-07-03 14:38:04 +0530

    Create arduino file for joystick stimulation

---
+++ joystickk.ino
@@ -0,0 +1,68 @@
+#define LEFT 3      //LEFT LED CONFIGURATION
+#define RIGHT 4     //RIGHT LED CONFIGURATION
+#define UP 5        //UP LED CONFIGURATION
+#define DOWN 2      //DOWN LED CONFIGURATION
+#define X_AXIS A0   //ANALOG INPUT OF X AXIS VALUE FROM JOYSTICK
+#define Y_AXIS A1   //ANALOG INPUT OF Y AXIS VALUE FROM JOYSTICK
+#define SW_pin A2   //SWITCH PIN OF JOYSTICK
+
+void setup() 
+{
+
+  pinMode(RIGHT, OUTPUT);   //SET RIGHT PIN AS OUTPUT
+  pinMode(LEFT, OUTPUT);    //SET LEFT PIN AS OUTPUT
+  pinMode(UP, OUTPUT);      //SET UP PIN AS OUTPUT
+  pinMode(DOWN, OUTPUT);    //SET DOWN PIN AS OUTPUT
+  pinMode(X_AXIS,INPUT);    //SET X-AXIS PIN OF JOYSTICK AS ANALOG INPUT
+  pinMode(Y_AXIS,INPUT);    //SET Y-AXIS PIN OF JOYSTICK AS ANALOG INPUT
+  pinMode(SW_pin, INPUT);   //SET SWITCH PIN OF JOYSTICK AS ANALOG INPUT
+
+  //Initialization
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  
+  //Initialize LED as LOW 
+  digitalWrite(UP,LOW);
+  digitalWrite(DOWN,LOW);
+  digitalWrite(RIGHT,LOW);
+  digitalWrite(LEFT,LOW);
+  
+  if(digitalRead(SW_pin)==HIGH)
+  {
+
+    int xread=analogRead(X_AXIS);     //READ VALUE OF X-AXIS FROM JOYSTICK
+    int yread=analogRead(Y_AXIS);     //READ VALUE OF Y-AXIS FROM JOYSTICK
+    Serial.print(xread);              //Print the values on screen
+    Serial.print("\	");
+    Serial.println(yread);
+
+    //According to the position of the joystick, turn LED on
+    if((xread >=0 && xread <= 1010) && (yread > 459 && yread <= 1010))
+    {
+      digitalWrite(UP,HIGH);
+    }
+
+    if((xread > 459 && xread <= 1010) && (yread >= 0 && yread <= 1010))
+    {
+      digitalWrite(LEFT,HIGH);
+    }
+
+    if((xread >=0 && xread <= 1010) && (yread >= 0 && yread < 459))
+    {
+      digitalWrite(DOWN,HIGH);
+    }
+
+    if((xread >=0 && xread < 459) && (yread >= 0 && yread <= 1010))
+    {
+      digitalWrite(RIGHT,HIGH);
+    }
+
+  }
+
+}
+
